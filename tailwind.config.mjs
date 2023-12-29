3ca85037c110d217cf6e0ad97bbf2d8d6431976a
John Angel <usw4763@gmail.com>
2023-12-29 21:24:50 +0100

    Added Footer components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			backgroundImage: {
-        'main': "url('/home-bg.jpg')",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        main: "url('/home-bg.jpg')",
+        "explore-beach": "url('/places/explore-beach.jpg')",
       },
-			fontFamily: {
-				'sans': ['Montserrat', 'Poppins']
-			},
-			textColor: {
-				'body': '#B3B3B3'
-			}
-		},
-	},
-	plugins: [],
-}
+      fontFamily: {
+        sans: ["Montserrat", "Poppins"],
+      },
+      textColor: {
+        body: "#B3B3B3",
+      },
+      color: {
+        "light-shadow": "#0009",
+        container: "#141414",
+      },
+      backgroundColor: {
+        container: "#141414",
+      },
+    },
+  },
+  plugins: [],
+};
