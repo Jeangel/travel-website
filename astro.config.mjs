948dfe978c5d29b2d7ecc7f7b8e7454658e9f55c
John Angel <usw4763@gmail.com>
2023-12-30 22:25:09 +0100

    Revert config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,4 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://jeangel.github.io',
-  base: '/travel-website',
 });
